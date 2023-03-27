8735f4ada10e15fbd2a9978d31f8768fcc87c6ea
corradoj02 <josephcorrado97@gmail.com>
2023-03-27 08:15:19 -0400

    [+] Moved/added files and folders. Added questions

--- index.js
+++ index.js
@@ -1,17 +1,104 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    // Title
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: validateInput,
+    },
+
+    // Description
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project.",
+        validate: validateInput,
+    },
+
+    // Installation
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter an explanation how to install the software, or commands for the program.",
+        validate: validateInput,
+    },
+
+    // Use
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe how we can use this program/project.",
+        validate: validateInput,
+    },
+
+    // Licensing
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for this project.",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Apache 2.0", "Boost Software 1.0", "MIT", "Mozilla"],
+        validate: validateInput,
+    },
+
+    // Contributions
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can users contribute to your project.",
+        validate: validateInput,
+    },
+
+    // Question for Tests
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter any testing instructions you would like to provide for this project.",
+        validate: validateInput,
+    },
+
+    // GitHub Username
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+        validate: validateInput,
+    },
+
+    // GitHub email address
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your GitHub email address so that contributors may contact you?",
+        validate: function (value) {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                return true;
+            } else {
+                return "Not a valid email address. Please enter a valid email address.";
+            }
+        },
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      });    
+}
 
 // Function call to initialize app
 init();
